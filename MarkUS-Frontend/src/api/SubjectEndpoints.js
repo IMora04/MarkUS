3020c776d486c6fee6be582aef0daa68720e25da
IMora04 <ignmorper1@alum.us.es>
2024-08-11 18:21:40 +0200

    New endpoint and subject fetch in subject Info Screen

--- MarkUS-Frontend/src/api/SubjectEndpoints.js
+++ MarkUS-Frontend/src/api/SubjectEndpoints.js
@@ -1,7 +1,11 @@
-import { post } from './helpers/ApiRequestsHelper'
+import { post, get } from './helpers/ApiRequestsHelper'
 
 function create (data) {
   return post('subjects', data)
 }
 
-export { create }
+function getDetail (id) {
+  return get(`subjects/${id}`)
+}
+
+export { create, getDetail }
--- MarkUS-Frontend/src/screens/SubjectInfoScreen.js
+++ MarkUS-Frontend/src/screens/SubjectInfoScreen.js
@@ -3,12 +3,13 @@ import { Text, StyleSheet, Dimensions, View, Pressable, Switch, ActivityIndicato
 import { FlatList } from 'react-native-gesture-handler'
 import * as GlobalStyles from '../styles/GlobalStyles'
 import { showMessage } from 'react-native-flash-message'
-import { getDetail } from '../api/CourseEndpoints'
+import { getDetail } from '../api/SubjectEndpoints'
 import * as yup from 'yup'
 
 export default function CourseInfoScreen ({ navigation, route }) {
   const [backendErrors, setBackendErrors] = useState()
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(true)
+  const [currentSubject, setCurrentSubject] = useState({})
 
   const validationSchema = yup.object().shape({
   })
@@ -31,13 +32,24 @@ export default function CourseInfoScreen ({ navigation, route }) {
     return () => subscription?.remove()
   })
 
-  useEffect(() => {
-    const fetchOneSubject = async (id) => {
-      try {
-      } catch (error) {
-      }
+  async function fetchOneSubject (id) {
+    try {
+      const fetchedSubject = await getDetail(id)
+      setCurrentSubject(fetchedSubject)
+    } catch (error) {
+      showMessage({
+        message: `There was an error while retrieving this subject. ${error} `,
+        type: 'error',
+        style: GlobalStyles.flashStyle,
+        titleStyle: GlobalStyles.flashTextStyle
+      })
     }
-    // fetchOneSubject(route.params.id)
+  }
+
+  useEffect(() => {
+    setLoading(true)
+    fetchOneSubject(route.params.id)
+    setLoading(false)
   }, [route])
 
   const renderSubject = ({ item }) => {
@@ -52,9 +64,12 @@ export default function CourseInfoScreen ({ navigation, route }) {
       <ActivityIndicator/>
     </View>
       : <ScrollView style={{ padding: 20 }}>
+      <Text style={{ textAlign: 'center', fontWeight: 600, fontSize: 20 }}>
+        {currentSubject.name}
+      </Text>
       <Text>
         {
-            JSON.stringify(route.params.currentCourse, null, '\t')
+            JSON.stringify(currentSubject, null, '\t')
         }
       </Text>
     </ScrollView>
