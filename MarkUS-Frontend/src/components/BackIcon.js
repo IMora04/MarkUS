60d0c9ede642fc03c58fe6a30f5f2dbd89b00078
IMora04 <ignmorper1@alum.us.es>
2024-08-18 20:53:17 +0200

    Back icon fixed

--- MarkUS-Frontend/src/components/BackIcon.js
+++ MarkUS-Frontend/src/components/BackIcon.js
@@ -1,6 +1,7 @@
 import React from 'react'
+import { Platform } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 export default function BackIcon (props) {
-  return <Icon name="chevron-left" size={30} {...props} />
+  return <Icon name="chevron-left" size={30} color={Platform.OS === 'ios' ? '#007AFF' : '#1a1a1a'} {...props} />
 }
