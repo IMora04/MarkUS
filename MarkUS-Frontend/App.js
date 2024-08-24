638a543f422c1f81452278a84f25e9835b2a77a6
IMora04 <ignmorper1@alum.us.es>
2024-08-24 16:37:24 +0200

    Added StudiesContext

--- MarkUS-Frontend/App.js
+++ MarkUS-Frontend/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AppContextProvider from "./src/context/AppContext";
 import AuthorizationContextProvider from "./src/context/AuthorizationContext";
+import StudiesContextProvider from "./src/context/StudiesContext";
 import Layout from "./src/screens/Layout";
 import * as WebBrowser from "expo-web-browser";
 
@@ -10,7 +11,9 @@ export default function App() {
   return (
     <AppContextProvider>
       <AuthorizationContextProvider>
-        <Layout />
+        <StudiesContextProvider>
+          <Layout />
+        </StudiesContextProvider>
       </AuthorizationContextProvider>
     </AppContextProvider>
   );
---
+++ MarkUS-Frontend/src/context/StudiesContext.js
@@ -0,0 +1,26 @@
+import React, { useState, createContext } from "react";
+
+const StudiesContext = createContext();
+
+const StudiesContextProvider = (props) => {
+  const [currentStudies, setCurrentStudies] = useState();
+  const [currentCourse, setCurrentCourse] = useState();
+  const [currentSubject, setCurrentSubject] = useState();
+
+  return (
+    <StudiesContext.Provider
+      value={{
+        currentStudies,
+        setCurrentStudies,
+        currentCourse,
+        setCurrentCourse,
+        currentSubject,
+        setCurrentSubject,
+      }}
+    >
+      {props.children}
+    </StudiesContext.Provider>
+  );
+};
+export { StudiesContext };
+export default StudiesContextProvider;
