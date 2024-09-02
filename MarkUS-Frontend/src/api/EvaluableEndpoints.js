e12af9df9e0d32540041403ce51f7046d284ffd3
IMora04 <ignaciomorap04@gmail.com>
2024-09-02 18:55:29 +0200

    New endpoint to create evaluable types

--- MarkUS-Frontend/src/api/EvaluableEndpoints.js
+++ MarkUS-Frontend/src/api/EvaluableEndpoints.js
@@ -8,6 +8,10 @@ function create(data) {
   return post("evaluable", data);
 }
 
+function createType(data) {
+  return post("evaluableTypes", data);
+}
+
 function update(id, data) {
   return put(`evaluable/${id}`, data);
 }
@@ -16,4 +20,4 @@ function remove(id, data) {
   return destroy(`evaluable/${id}`);
 }
 
-export { getAll, create, update, remove };
+export { getAll, create, update, remove, createType };
